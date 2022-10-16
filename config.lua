05aa53da6d6f2db08dd49f9c78bfd2876f7b790b
XxJxsperxX <115908642+XxJxsperxX@users.noreply.github.com>
2022-10-15 21:35:32 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,41 @@
+Apartments = {}
+Apartments.Starting = true
+Apartments.SpawnOffset = 30
+Apartments.Locations = {
+    ["apartment1"] = {
+        name = "apartment1",
+        label = "Tug Street",
+        coords = {
+            enter = vector4(-969.78, -1431.33, 7.68, 293.84)
+        },
+        polyzoneBoxData = {
+            heading = 116.78,
+            minZ = 30,
+            maxZ = 33.5,
+            debug = true,
+            length = 1,
+            width = 1,
+            distance = 2.0,
+            created = false
+        },
+        price = 600,
+    },
+    ["apartment2"] = {
+        name = "apartment2",
+        label = "N. Archer Ave",
+        coords = {
+            enter = vector4(-71.06, 142.35, 81.67, 212.8)
+        },
+        polyzoneBoxData = {
+            heading = 116.78,
+            minZ = 30,
+            maxZ = 33.5,
+            debug = true,
+            length = 1,
+            width = 1,
+            distance = 2.0,
+            created = false
+        },
+        price = 1200,
+    },
+}
